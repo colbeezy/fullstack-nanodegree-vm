86ef4a27f6db85f9ae5682db83a8aa3a827d3ad4
Branden <branden.h.sanders@vanderbilt.edu>
2017-10-08 20:44:21 -0400

    Create database with tables for users, categories, and items

---
+++ vagrant/catalog/database_setup.py
@@ -0,0 +1,63 @@
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+# Create table of users
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    profile_image = Column(String(250))
+
+
+# Create table of categories
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'id': self.id,
+            'name': self.name,
+        }
+
+
+# Create table of items
+class Item(Base):
+    __tablename__ = 'item'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(350), nullable=False)
+    description = Column(String(1000))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+        }
+
+
+# Create database "itemcatalog.db" and add the tables from the classes above
+engine = create_engine('sqlite:///itemcatalog.db')
+
+Base.metadata.create_all(engine)
